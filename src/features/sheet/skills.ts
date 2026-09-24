afab0dd5c9c3315e055b112bc92b339ebbcda73e
Felipe Bellotto <pixpyp@gmail.com>
2026-09-24 15:33:28 -0300

    feat: add skill definitions

---
+++ src/features/sheet/skills.ts
@@ -0,0 +1,2 @@
+export const SKILLS=["Acrobacia","Adestramento","Artes","Atletismo","Atualidades","Ciências","Crime","Diplomacia","Enganação","Fortitude","Furtividade","Iniciativa","Intimidação","Intuição","Investigação","Luta","Medicina","Ocultismo","Percepção","Pilotagem","Pontaria","Profissão","Reflexos","Religião","Sobrevivência","Tática","Tecnologia","Vontade"] as const;
+export type Skill={name:string;bonus:number;training:boolean};
